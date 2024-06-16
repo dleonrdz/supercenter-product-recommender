42e5c1e2478b322da1a69377adb12008a0b19a23
Diego León <diego.leon07@outlook.com>
2024-06-16 17:27:34 +0200

    retrieval proces updated

--- supercenter-product-recommender/retrievers.py
+++ supercenter-product-recommender/retrievers.py
@@ -1,6 +1,8 @@
 from sklearn.metrics.pairwise import cosine_similarity
 from vector_storage import load_faiss_index
 import numpy as np
+from db_utilities import read_table
+from embedding_process import data_preparation
 
 def faiss_retrieval(db_path, transformer_name, k=5):
     db = load_faiss_index(path=db_path, transformer=transformer_name)
@@ -18,3 +20,19 @@ def get_top_n_recommendations(similarity_matrix, product_ids, n=5):
         top_n_product_ids = [product_ids[i] for i in top_n_indices]
         recommendations.append((idx, top_n_product_ids))
     return recommendations
+
+
+if __name__ == "__main__":
+    orders_df = read_table('processed_orders_data')
+    products_df = read_table('processed_products_data')
+    ord_cols = ['product_id', 'product_name', 'department']
+    prod_cols = ['product_name', 'department']
+    df_orders, df_products = data_preparation(orders_df,
+                                              ord_cols,
+                                              products_df,
+                                              prod_cols)
+    df_orders = df_orders.head(1)
+    query = df_orders['text_feature'].iloc[0]
+    retriever = faiss_retrieval('vectorstores/products_index_pt', 'hiiamsid/sentence_similarity_spanish_es')
+    response = retriever.invoke(query)
+    print(response)
