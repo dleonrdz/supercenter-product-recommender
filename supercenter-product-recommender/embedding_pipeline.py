71be064c3b5d11ee1baf39278908c515c744ff65
Diego León <diego.leon07@outlook.com>
2024-06-17 20:47:21 +0200

    embedding pipeline updated

--- supercenter-product-recommender/embedding_pipeline.py
+++ supercenter-product-recommender/embedding_pipeline.py
@@ -1,5 +1,5 @@
 from db_utilities import read_table
-from embedding_process import data_preparation, embedding_process
+from embedding_process import data_preparation_orders, data_preparation_products, embedding_process
 from vector_storage import create_faiss_index
 import numpy as np
 
@@ -11,10 +11,11 @@ orders_df['order_id'] = orders_df['order_id'].astype(str)
 print('Processing data...')
 ord_cols = ['product_id', 'product_name', 'department']
 prod_cols = ['product_id', 'product_name', 'department']
-df_orders, df_products = data_preparation(orders_df,
-                                          ord_cols,
-                                          products_df,
-                                          prod_cols)
+df_orders = data_preparation_orders(orders_df,
+                                    ord_cols)
+
+df_products = data_preparation_products(products_df,
+                                        prod_cols)
 
 print('Creating vectorstores...')
 create_faiss_index(df=df_products,
--- supercenter-product-recommender/embedding_process.py
+++ supercenter-product-recommender/embedding_process.py
@@ -4,32 +4,28 @@ import os
 # Calculate the path to the directory ABOVE the current script directory (i.e., the project root)
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def data_preparation(orders, orders_features, products, products_features):
+def data_preparation_orders(df_orders, features):
 
-  orders['text_feature'] = orders[orders_features]\
+  df_orders['text_feature'] = df_orders[features]\
     .apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
-  df_orders = orders.groupby('order_id',
+  df_orders = df_orders.groupby('order_id',
                                 as_index=False)['text_feature'].agg(lambda x: ' '.join(x))
 
-  products['text_feature'] = products[products_features]\
+  return df_orders[['order_id', 'text_feature']]
+
+def data_preparation_products(df_products, features):
+  df_products['text_feature'] = df_products[features]\
     .apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
 
-  return df_orders[['order_id','text_feature']], products[['product_id','text_feature']]
+  return df_products[['product_id','text_feature']]
 
-def embedding_process(df_orders,df_products, transformer):
+def embedding_process(df, transformer):
   path = os.path.join(PROJECT_ROOT, 'data/')
   model = SentenceTransformer(transformer)
 
-  order_embeddings = model.encode(df_orders['text_feature'].tolist())
-  product_embeddings = model.encode(df_products['text_feature'].tolist())
+  embeddings = model.encode(df['text_feature'].tolist())
 
-  df_orders['embedding'] = order_embeddings.tolist()
-  df_products['embedding'] = product_embeddings.tolist()
-
-  df_orders.to_json(f'{path}orders_embeddings_pt.json', orient='records', lines=True)
-  df_products.to_json(f'{path}products_embeddings_pt.json', orient='records', lines=True)
-
-  return order_embeddings, product_embeddings, model
+  return embeddings
 
 
 
