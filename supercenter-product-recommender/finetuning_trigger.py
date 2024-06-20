90da199c8405119998afa2c80523cf2e9cd969b7
Diego León <diego.leon07@outlook.com>
2024-06-20 11:44:49 +0200

    finetuning process fixes

--- supercenter-product-recommender/finetuning_trigger.py
+++ supercenter-product-recommender/finetuning_trigger.py
@@ -30,8 +30,6 @@ print('Retrieving orders embeddings...')
 orders_embeddings = index_orders.reconstruct_n(0, index_orders.ntotal)
 orders_embeddings = np.array(orders_embeddings)
 
-print(len(orders_embeddings))
-
 with open('vectorstores/orders_index_pt/index.pkl', 'rb') as f:
     index_content = pickle.load(f)
 
@@ -42,21 +40,38 @@ order_id_to_index = {v: k for k, v in index_to_order_id.items()}
 train_indices = [order_id_to_index[id] for id in train_order_ids if id in order_id_to_index]
 train_order_embeddings = np.array(orders_embeddings[train_indices])
 
-print(len(train_order_embeddings))
-
 #print('Creating training orders embeddings...')
 #train_order_embeddings = embedding_process(train_orders_df_prepared, transformer_name)
 #train_order_embeddings = np.array(train_order_embeddings)
 
 print('Preparing for training...')
-embedding_dim = 768  # Assuming the desired output embedding dimension is the same as input dimension
-model = TwoTowerModel(embedding_dim=embedding_dim)
+product_embedding_dim = 768
+order_embedding_dim = 768
+model = TwoTowerModel(product_embedding_dim, order_embedding_dim)
 optimizer = tf.keras.optimizers.Adam()
 
 print('Training...')
 epochs = 10
 batch_size = 32
 
+for epoch in range(epochs):
+    print(f"Epoch {epoch + 1}/{epochs}")
+    for i in range(0, len(product_embeddings), batch_size):
+        product_batch = product_embeddings[i:i + batch_size]
+        order_batch = train_order_embeddings[i:i + batch_size]
+
+        if product_batch.shape[0] != order_batch.shape[0]:
+            min_batch_size = min(product_batch.shape[0], order_batch.shape[0])
+            product_batch = product_batch[:min_batch_size]
+            order_batch = order_batch[:min_batch_size]
+
+        loss = train_step(model, product_batch, order_batch, optimizer)
+        print(f"Batch {i // batch_size + 1}/{len(product_embeddings) // batch_size}: Loss = {loss.numpy()}")
+
+print('Saving model...')
+model.save('models/two_towers_trained.keras')
+
+'''
 for epoch in range(epochs):
     print(f"Epoch {epoch + 1}/{epochs}")
     for i in range(0, min(len(product_embeddings), len(train_order_embeddings)), batch_size):
@@ -70,5 +85,4 @@ for epoch in range(epochs):
         loss = train_step(model, product_batch, order_batch, optimizer)
         print(f"Batch {i // batch_size + 1}/{len(product_embeddings) // batch_size}: Loss = {loss.numpy()}")
 
-#Saving model
-model.save('models/two_towers_trained.keras')
+'''
